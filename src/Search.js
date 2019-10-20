a46e907b722b2d4aa1b3ca3665f843b49209c235
Baptiste <baptiste.puccinelli@gmail.com>
2019-10-20 11:47:12 +0200

    Input display query

--- src/Search.js
+++ src/Search.js
@@ -1,25 +1,32 @@
 import React from 'react';
+import axios from 'axios';
 
 class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       query: '',
-      results: {},
-      loading: false,
-      message: ''
+      results: {}
     };
   }
 
+  handleOnInputChange = event => {
+    const query = event.target.value;
+    this.setState({ query: query });
+  };
+
   render() {
+    const { query } = this.state;
     return (
       <div className='container'>
         <h1>Marvel Super-Heroes Search</h1>
         <label className='search-label'>
           <input
             type='text'
-            value=''
+            name='query'
+            value={query}
             id='search-input'
+            onChange={this.handleOnInputChange}
             placeholder='Search heroes...'
           />
         </label>
