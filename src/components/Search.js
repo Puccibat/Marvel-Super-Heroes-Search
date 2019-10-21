a9b68f0e390127e093b20b4d9c0b62520582af74
Baptiste <baptiste.puccinelli@gmail.com>
2019-10-21 14:15:13 +0200

    Display modal ok

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import '../style/Search.css';
-import {Card} from 'react-bootstrap';
+import {Card, Modal, Button, ButtonToolbar} from 'react-bootstrap';
 
 class Search extends React.Component {
   constructor(props) {
@@ -67,6 +67,51 @@ class Search extends React.Component {
     }
   };
 
+  heroModal = (props) => {
+  return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Modal heading
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Centered Modal</h4>
+          <p>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+            consectetur ac, vestibulum at eros.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
+  detailsButton = () => {
+    const [modalShow, setModalShow] = React.useState(false);
+    return (
+      <ButtonToolbar>
+        <Button variant="primary" onClick={() => setModalShow(true)}>
+          DETAILS
+        </Button>
+
+        <this.heroModal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+        />
+      </ButtonToolbar>
+    );
+  }
+
+
   renderSearchHeroes = () => {
     const { heroes, isSearchable, loading } = this.state;
     if (loading) {
@@ -84,10 +129,11 @@ class Search extends React.Component {
             {heroes.map(result => {
               return (
                 <div key={result.id}>
-                    <Card style={{ width: '15rem' }}>
+                    <Card className="heroesCards" style={{ width: '15rem' }}>
                       <Card.Body>
-                        <Card.Img variant="top" src={`${result.thumbnail.path}.${result.thumbnail.extension}`} />
-                        <Card.Title><h6>{result.name}</h6></Card.Title>
+                        <Card.Img variant="top" src={`${result.thumbnail.path}.${result.thumbnail.extension}`} className="thumbnail" />
+                        <Card.Title><h2>{result.name}</h2></Card.Title>
+                        <this.detailsButton />
                       </Card.Body>
                     </Card>
                 </div>
