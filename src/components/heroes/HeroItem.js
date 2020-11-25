83a30a5c5f2ed25d0a3ff8ca6e787736d44bdc09
Baptiste <baptiste.puccinelli@gmail.com>
2020-11-25 14:28:38 +0100

    Hero card: name & description & image OK

--- src/components/heroes/HeroItem.js
+++ src/components/heroes/HeroItem.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 const HeroItem = ({ hero }) => {
   console.log(hero);
   return (
-    <div>
-      {hero.name}
+    <div className='card'>
+      <div className='card-inner'>
+        <div className='card-front'>
+          <img
+            src={`${hero.thumbnail.path}.${hero.thumbnail.extension}`}
+            alt=''
+          />
+        </div>
+        <div className='card-back'>
+          <h1>{hero.name}</h1>
+          <p>{hero.description}</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeroItem
+export default HeroItem;
